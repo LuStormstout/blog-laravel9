7fcde9e8cc1eb9580cb0243bcfafe22eba9f7267
Lu Stormstout <lustormstout@gmail.com>
2024-08-08 09:22:15 +0800

    定义静态页面

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// 首页
+Route::get('/', 'StaticPagesController@home')->name('/');
+
+// 帮助页面
+Route::get('/help', 'StaticPagesController@help')->name('help');
+
+// 关于页面
+Route::get('/about', 'StaticPagesController@about')->name('about');
