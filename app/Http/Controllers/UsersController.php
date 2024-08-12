923934ad3c660df135433f6efacbb6f3ca3dc467
Lu Stormstout <lustormstout@gmail.com>
2024-08-12 08:26:56 +0800

    新增用户注册表单

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -10,11 +10,22 @@ use Illuminate\Http\Request;
 
 class UsersController extends Controller
 {
+    /**
+     * 显示用户列表
+     *
+     * @return Factory|View|Application
+     */
     public function create(): Factory|View|Application
     {
         return view('users.create');
     }
 
+    /**
+     * 显示用户个人信息
+     *
+     * @param User $user
+     * @return Factory|View|Application
+     */
     public function show(User $user): Factory|View|Application
     {
         return view('users.show', compact('user'));
