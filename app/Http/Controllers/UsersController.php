e2b6aba73b55511c2dcd12d6aa4d2f82970cc357
Lu Stormstout <lustormstout@gmail.com>
2024-08-12 09:27:19 +0800

    完成了用户注册的功能

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -6,6 +6,7 @@ use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Contracts\View\Factory;
 use Illuminate\Contracts\View\View;
 use App\Models\User;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Validation\ValidationException;
 
@@ -33,9 +34,13 @@ class UsersController extends Controller
     }
 
     /**
+     * 创建用户
+     *
+     * @param Request $request
+     * @return RedirectResponse
      * @throws ValidationException
      */
-    public function store(Request $request): void
+    public function store(Request $request): RedirectResponse
     {
         $this->validate($request, [
             'name' => 'required|unique:users|max:50',
@@ -43,7 +48,14 @@ class UsersController extends Controller
             'password' => 'required|confirmed|min:6'
         ]);
 
-        return;
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password)
+        ]);
+
+        session()->flash('success', '注册成功！');
+        return redirect()->route('users.show', [$user]);
     }
 
 }
