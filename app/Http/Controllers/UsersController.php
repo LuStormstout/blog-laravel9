d7aa0563fd96bbe882f86e6051ef3b115fd3486c
Lu Stormstout <lustormstout@gmail.com>
2024-08-16 09:26:57 +0800

    用户微博列表

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -54,7 +54,10 @@ class UsersController extends Controller
      */
     public function show(User $user): Factory|View|Application
     {
-        return view('users.show', compact('user'));
+        $statuses = $user->statuses()
+            ->orderBy('created_at', 'desc')
+            ->paginate(10);
+        return view('users.show', compact('user', 'statuses'));
     }
 
     /**
