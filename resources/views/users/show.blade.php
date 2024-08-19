77f99df5f46d5a31cb7e8c98cad509222f3fa6b2
Lu Stormstout <lustormstout@gmail.com>
2024-08-19 11:07:46 +0800

    关注按钮

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -1,3 +1,4 @@
+@php use Illuminate\Support\Facades\Auth; @endphp
 @extends('layouts.default')
 @section('title', $user->name)
 
@@ -9,6 +10,11 @@
                     <section class="user_info">
                         @include('shared._user_info', ['user' => $user])
                     </section>
+
+                    @if(Auth::check())
+                        @include('users._follow_form')
+                    @endif
+
                     <section class="stats mt-2">
                         @include('shared._stats', ['user' => $user])
                     </section>
